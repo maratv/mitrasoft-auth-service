ba493b645a1b04cadc12d0ce62d0ab0c2252bb7f
Marat Vakhitov <404@bk.ru>
2023-07-04 22:55:35 +0300

    pass encoder added

--- src/main/java/com/example/mitrasoftauthservice/config/WebClientAuthenticationProvider.java
+++ src/main/java/com/example/mitrasoftauthservice/config/WebClientAuthenticationProvider.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 public class WebClientAuthenticationProvider implements AuthenticationProvider {
 
     private final UserClient userClient;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -24,7 +26,7 @@ public class WebClientAuthenticationProvider implements AuthenticationProvider {
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
 
-        if (user != null && user.getEmail().equals(name) && user.getPassword().equals(password)) {
+        if (user != null && user.getEmail().equals(name) && passwordEncoder.matches(password, user.getPassword())) { // user.getPassword().equals(password)) {
             return new UsernamePasswordAuthenticationToken(
                     name, password, new ArrayList<>());
         } else {
