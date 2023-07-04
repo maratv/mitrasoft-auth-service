c861933c0058c95291506134789f03b4dd7dddc0
Marat Vakhitov <404@bk.ru>
2023-07-04 16:50:38 +0300

    birthday field added, pom cleared

--- src/main/java/com/example/mitrasoftauthservice/controller/UserServiceController.java
+++ src/main/java/com/example/mitrasoftauthservice/controller/UserServiceController.java
@@ -3,13 +3,11 @@ package com.example.mitrasoftauthservice.controller;
 import com.example.mitrasoftauthservice.dto.UserDto;
 import com.example.mitrasoftauthservice.service.WebClientService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
@@ -28,16 +26,9 @@ public class UserServiceController {
     @GetMapping("user/get/{email}")
     public ResponseEntity<?> getUserDtoByEmail(@PathVariable String email) {
 
-        try {
-            UserDto userDto = webClientService.getUserDtoByEmail(email);
-            return ResponseEntity.ok(userDto);
-        } catch (WebClientResponseException ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("user with email '" + email + "' not found");
-        }
+        UserDto userDto = webClientService.getUserDtoByEmail(email);
+        return ResponseEntity.ok(userDto);
     }
-
-
-
-
 }
+
+
--- src/main/java/com/example/mitrasoftauthservice/dto/UserDto.java
+++ src/main/java/com/example/mitrasoftauthservice/dto/UserDto.java
@@ -3,6 +3,8 @@ package com.example.mitrasoftauthservice.dto;
 import com.example.mitrasoftauthservice.domain.UserRole;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class UserDto {
     private String email;
@@ -14,4 +16,6 @@ public class UserDto {
     private String firstName;
 
     private String lastName;
+
+    private LocalDate birthday;
 }
